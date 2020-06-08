bc8726b9b6183072fa7b3058ee436d524adc057c
Jan <33544659+jeepeegit@users.noreply.github.com>
2020-06-08 12:08:17 +0200

    Update bin_api_keys.py

--- bin_api_keys.py
+++ bin_api_keys.py
@@ -1,4 +1,4 @@
-# API key's. Put here your keys: PUBLIC = "key" and SECRET = "key"
+# API key's. Put here your keys: PUBLIC = "key" and SECRET = "key" (read only)
 # keys are NOT enqrypted !!!!
 PUBLIC = 'publickey'
 SECRET = 'secretkey'
